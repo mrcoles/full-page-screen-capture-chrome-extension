89165e06d06185151ae9c200ed1731bbba802f15
Benjamin Gleitzman <gleitz@mit.edu>
2012-11-06 14:56:23 -0800

    Updated the extension to create a temporary file called screenshot.png that can be saved with right click -> save file as... Removed the dependence on the regular expression match when converting dataURI to blob. Updated README to explain how to install the crx into Chrome.

--- popup.js
+++ popup.js
@@ -55,26 +55,39 @@ function openPage() {
     // standard dataURI can be too big, let's blob instead
     // http://code.google.com/p/chromium/issues/detail?id=69227#c27
 
-    // take apart data URL
-    var parts = screenshot.canvas.toDataURL().match(/data:([^;]*)(;base64)?,([0-9A-Za-z+/]+)/);
+    var dataURI = screenshot.canvas.toDataURL();
 
-    // assume base64 encoding
-    var binStr = atob(parts[3]);
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs
+    var byteString = atob(dataURI.split(',')[1]);
 
-    // convert to binary in ArrayBuffer
-    var buf = new ArrayBuffer(binStr.length);
-    var view = new Uint8Array(buf);
-    for (var i = 0; i < view.length; i++) {
-        view[i] = binStr.charCodeAt(i);
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
     }
 
-    var builder = new WebKitBlobBuilder();
-    builder.append(buf);
+    // write the ArrayBuffer to a blob, and you're done
+    var bb = new window.WebKitBlobBuilder();
+    bb.append(ab);
 
-    // create blob with mime type, create URL for it
-    var URL = webkitURL.createObjectURL(builder.getBlob(parts[1]));
+    // create a blob for writing to a file
+    var blob = bb.getBlob(mimeString);
+    window.webkitRequestFileSystem(TEMPORARY, 1024*1024, function(fs){
+        console.log(fs);
+        fs.root.getFile("screenshot.png", {create:true}, function(fileEntry) {
+            fileEntry.createWriter(function(fileWriter) {
+                fileWriter.write(blob);
+            }, function(e) {console.log(e);});
+        }, function(e) {console.log(e);});
+    }, function(e) {console.log(e);});
 
-    window.open(URL);
+    // open the file that now contains the blob
+    window.open('filesystem:chrome-extension://' + chrome.i18n.getMessage("@@extension_id") + '/temporary/screenshot.png');
 }
 
 // start doing stuff immediately!
